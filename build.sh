7a7be2ca689d1446f03a92311bb3ffdfa4f0d949
sumxtx <137871537+sumxtx@users.noreply.github.com>
2025-02-10 18:53:58 +0100

    updated build instructions

--- build.sh
+++ build.sh
@@ -2,5 +2,11 @@ mkdir build && cd build
 cmake ../
 cmake --build .
 
-# ./tools/yad #execute
+# Add capabilities to yad to trace non-child processes 
+# cd tools && setcap CAP_SYS_PTRACE=+eip yad.
+
+# or globally allow PTRACE_ATTACH
+# sudo echo 0 > /proc/sys/kernel/yama/ptrace_scope
+
+# .{YAD_ROOT}/build/tools/yad # bin location
 
